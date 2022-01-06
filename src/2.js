c928e548f1f95c52c179561ac37473a5c4f3e739
鲁举佩 <653390161@qq.com>
2022-01-06 20:39:36 +0800

    feat: 2

--- src/2.js
+++ src/2.js
@@ -2,7 +2,31 @@
 // 函数真没什么特殊的，
 // 你可以像对待任何其他数据类型一样对待它们——把它们存在数组里，
 // 当作参数传递，赋值给变量...等等。
-const hi = name => `Hi ${name}`;
-const greeting = name => hi(name);
 
-hi('ljp');
+{
+  const hi = name => `Hi ${name}`;
+  const greeting = name => hi(name);
+  console.log(
+    hi('ljp')
+  );
+}
+
+{
+  // 只针对当前的博客
+  const validArticles = articles =>
+    articles.filter(article => article !== null && article !== undefined);
+
+  // 对未来的项目更友好
+  const compact = xs => xs.filter(x => x !== null && x !== undefined);
+}
+
+// 注意this值
+{
+  var fs = require('fs');
+
+  // 太可怕了
+  fs.readFile('freaky_friday.txt', Db.save);
+
+  // 好一点点
+  fs.readFile('freaky_friday.txt', Db.save.bind(Db));
+}
